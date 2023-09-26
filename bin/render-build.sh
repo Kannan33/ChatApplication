8f597f49489d94cdb78decf32e686dedbad52c72
Kanna33 <kannankm69879@gmail.com>
2023-09-26 17:33:30 +0530

    production on render 4

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-sudo gem install bundler
-bundle install
+
+bundle update
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
