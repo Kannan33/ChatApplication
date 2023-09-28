e72ed283d5a49a28be8393bd5f67d75e36c01c33
Kanna33 <kannankm69879@gmail.com>
2023-09-28 15:40:24 +0530

    Production error fixed 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,23 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users, only: [:index] do
-    # for profile
+    # search user profile
     get 'profile', to: 'users#search_profile', on: :member
     get 'profile/:id', to: 'users#profile'
+    # manage users conversations
     get 'manage_users', to: 'users#manage_users', on: :member
     # for remove user from chat list
     resources :conversations, only: [:show, :create, :destroy ] do
+      # messages unread count reset to 0 if user is viewed
       post :unread_messages_reset, on: :member, as: :unread_messages_reset
       resources :messages, only: [:create]
     end
   end
+  # current user profile
   get 'profile', to: 'users#profile'
 
   # Defines the root path route ("/")
   root "users#index"
-  # redirect to root page if undefined path
-  match '*unmatched', to: 'application#not_found_method', via: :all
+  # redirect to root page if undefined path and add constraints except active storage routes [to handle AWS S3 requests]
+  match '*unmatched', to: 'application#not_found_method', via: [:get, :post, :put, :patch, :delete], constraints: lambda { |req| req.path.exclude?('/rails/active_storage') }
 end
